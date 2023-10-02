243b96176294ab31e9ab7633cb190521f0b9366e
Keshav Prasad <keshavprasadms@gmail.com>
2023-10-03 00:00:29 +0530

    fix: changes to ns vars

--- terraform/modules/helm/postgresql_migration/main.tf
+++ terraform/modules/helm/postgresql_migration/main.tf
@@ -1,17 +1,17 @@
 resource "helm_release" "postgresql_migration" {
     name             = var.postgresql_migration_release_name
     chart            = "${path.module}/${var.postgresql_migration_chart_path}"
-    namespace        = var.postgresql_migration_namespace
+    namespace        = var.postgresql_namespace
     wait_for_jobs    = var.postgresql_migration_wait_for_jobs
     timeout          = var.postgresql_migration_install_timeout
     depends_on       = [var.postgresql_migration_chart_depends_on]
-    force_update      = true
-    cleanup_on_fail   = true
-    atomic            = true
+    force_update     = true
+    cleanup_on_fail  = true
+    atomic           = true
     values = [
         templatefile("${path.module}/${var.postgresql_migration_custom_values_yaml}",
         {
-          postgresql_migration_namespace     = var.postgresql_migration_namespace
+          namespace                          = var.postgresql_namespace
           postgresql_url                     = var.postgresql_url
           postgresql_admin_username          = var.postgresql_admin_username
           postgresql_admin_password          = var.postgresql_admin_password
