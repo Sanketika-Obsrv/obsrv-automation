7c4b4ae5b6d9eae1a2e74bd976ecf43736fa6082
Manjunath Davanam <manjunath@sanketika.in>
2023-12-15 14:48:57 +0530

    Release 1.3.1 Changes (#109)
    * #90 fix: Add default value for published_date in tables (#94)
    
    * fix: remove restart policy from flink deployment manifest (#96)
    
    * #196 Fix:Remove default region. (#95)
    
    * fix: #196 remove default value for region
    
    * update: #196 missing variable in variables.tf file
    
    * #196 update:  example on how provide region
    
    * #196 update: comments on how to provide region
    
    * Separation of redis into dedup & meta/denorm. (#92)
    
    * feat: Separation of redis into dedup & meta/denorm.
    
    * feat: Separation of redis into dedup & meta/denorm.
    
    * feat: Separation of redis into dedup & meta/denorm.
    
    * feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #58 feat: Separation of redis into dedup & meta/denorm.
    
    * #126 feat: add new property to datasources table (#99)
    
    * #126 feat: add new property to datasources table
    
    * #126 fix: allow property to be nullable
    
    * #0000 - feat: Fix masterdata stats ingestion spec (#100)
    
    * #0000 - feat: Fix masterdata stats ingestion spec
    * #0000 - feat: Add latency and processing times
    
    * #0000 - feat: Flink helm configuration changes (#103)
    
    * #0000 - feat: Flink helm configuration changes
    
    * #0000 - feat: Ingestion spec changes for system-events
    
    * #0000 - feat: Remove outdated ingestion specs
    
    * #0000 - feat: Secor changes for unified failed topic (#104)
    
    * feat: azure terraform changes to provision cluster (#106)
    
    * feat: azure terraform changes to provision cluster
    
    * cleanup azure terraform code
    
    * update node count and instance size for azure (#107)
    
    * Issue #305 feat: exclude system-events for the datasource validation (#108)
    
    * Issue #0000 fix: system events ingestion spec typo fix (#105)
    
    * Issue #0000 fix: system events ingestion spec typo fix
    
    * Issue #0000 fix: system-events ingestion spec update to add dataset_type
    
    * #0 fix: add unique relation for source config table (#110)
    
    ---------
    
    Co-authored-by: Praveen Veleneni <66662436+pveleneni@users.noreply.github.com>
    Co-authored-by: Manoj Krishna <92361832+ManojKrishnaChintaluri@users.noreply.github.com>
    Co-authored-by: Sowmya N Dixit <sowmyadixit7@gmail.com>
    Co-authored-by: Ravi Mula <ravishankarmula@gmail.com>
    Co-authored-by: Anand Parthasarathy <anandp504@gmail.com>
    Co-authored-by: Ravinder Kumar <yravinderkumar33@gmail.com>

--- terraform/modules/helm/postgresql_migration/postgresql-migration-helm-chart/migration/03_obsrv/V1__create_obsrv_initial_data.sql
+++ terraform/modules/helm/postgresql_migration/postgresql-migration-helm-chart/migration/03_obsrv/V1__create_obsrv_initial_data.sql
@@ -55,6 +55,7 @@ CREATE TABLE IF NOT EXISTS datasources (
   archival_policy json,
   purge_policy json,
   backup_config json NOT NULL,
+  metadata json,
   status text NOT NULL,
   created_by text NOT NULL,
   updated_by text NOT NULL,
@@ -97,7 +98,8 @@ CREATE TABLE IF NOT EXISTS dataset_source_config (
   updated_by text NOT NULL,
   created_date TIMESTAMP NOT NULL DEFAULT now(),
   updated_date TIMESTAMP NOT NULL,
-  published_date TIMESTAMP NOT NULL DEFAULT now()
+  published_date TIMESTAMP NOT NULL DEFAULT now(),
+  UNIQUE(connector_type, dataset_id)
 );
 
 CREATE INDEX IF NOT EXISTS  dataset_source_config_dataset ON dataset_source_config(dataset_id);
