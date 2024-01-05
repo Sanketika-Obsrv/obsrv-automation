4b7035476e0185e8fbd0df7f880110c204f1e77b
Manjunath Davanam <manjunath@sanketika.in>
2024-01-05 13:20:18 +0530

    #241:feat: Updated the infra setup shell script

--- automation-scripts/infra-setup/setup.sh
+++ automation-scripts/infra-setup/setup.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-# Function to check if a command is available
-check_command() {
-    command -v "$1" > /dev/null 2>&1
+version_compare() {
+    local version1="$1"
+    local version2="$2"
+    IFS='.' read v1_major v1_minor v1_patch <<< "$version1"
+    IFS='.' read v2_major v2_minor v2_patch <<< "$version2"
+
+    if [ "$v1_major" -gt "$v2_major" ] || [ "$v1_major" -eq "$v2_major" -a "$v1_minor" -gt "$v2_minor" ] || [ "$v1_major" -eq "$v2_major" -a "$v1_minor" -eq "$v2_minor" -a "$v1_patch" -ge "$v2_patch" ]; then
+        echo true  # version1 is greater than or equal to version2
+    else
+        echo false  # version1 is less than version2
+    fi
+}
+
+# Function to get installed version
+get_installed_version() {
+    local version_command="$1"
+    if [ -n "$version_command" ]; then
+        installed_version=$(eval "$version_command")
+        echo "$installed_version"
+    else
+        echo "0.0.0" 
+    fi
 }
 
 # Function to install a tool
@@ -10,61 +29,81 @@ install_tool() {
     local tool_name="$1"
     local install_command="$2"
     local version_command="$3"
+    local required_version="$4"
 
-    echo "Installing $tool_name..."
-    eval "$install_command"
-
-    # Check if the installation was successful
-    if [ $? -eq 0 ]; then
-        echo "$tool_name installed successfully."
-        if [ -n "$version_command" ]; then
-            installed_version=$(eval "$version_command")
-            echo "Version: $installed_version"
-        fi
+    installed_version=$(get_installed_version "$version_command")
+    compare_result=$(version_compare "$installed_version" "$required_version")
+    if [ "$compare_result" == "true" ]; then
+        echo "$tool_name is already installed with supported version $installed_version"
+        return
     else
-        echo "Error: Failed to install $tool_name. Please install it manually before proceeding."
-        exit 1
-    fi
+        echo "$tool_name tool version $required_version is missing, but the installed version is $installed_version. Would you like to install the stable version of $tool_name? (yes/no)"
+        read -r response
+
+        if [ "$response" == "yes" ]; then
+            echo "Installing $tool_name..."
+            eval "$install_command"
+
+            # Check if the installation was successful
+            if [ $? -eq 0 ]; then
+                echo "$tool_name installed successfully."
+                if [ -n "$version_command" ]; then
+                    installed_version=$(get_installed_version "$version_command")
+                    echo "Version: $installed_version"
+                fi
+            else
+                echo "Error: Failed to install $tool_name. Please install it manually before proceeding."
+                exit 1
+            fi
+        else
+            echo "Skipping installation of $tool_name."
+        fi
+    fi    
 }
+
 
 # Validate and install required tools
 validate_tools() {
-    # Check for required tools
-    tools=("aws" "terraform" "terrahelp" "terragrunt" "kubectl")
+    # Define all the required tools with version
+    tool_versions=(
+        "aws:2.13.8"
+        "helm:3.50.2"
+        "terraform:1.5.7"
+        "terrahelp:0.7.5"
+        "terragrunt:0.45.6"
+    )
 
-    for tool in "${tools[@]}"; do
-        if ! check_command "$tool"; then
-            echo "$tool not found. Do you want to install $tool? (yes/no)"
-            read -r response
-
-            if [ "$response" == "yes" ]; then
-                case $tool in
-                    "aws")
-                        install_tool "$tool" 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install && rm -rf awscliv2.zip aws' 'aws --version'
-                        ;;
-                    "terraform")
-                        install_tool "$tool" 'curl "https://releases.hashicorp.com/terraform/1.5.2/terraform_1.5.2_linux_amd64.zip" -o "terraform.zip" && unzip terraform.zip && mv terraform /usr/local/bin/ && rm terraform.zip' 'terraform --version'
-                        ;;
-                    "terrahelp")
-                        install_tool "$tool" 'curl -OL https://github.com/opencredo/terrahelp/releases/download/v0.4.3/terrahelp-linux-amd64 && mv terrahelp-linux-amd64 /usr/local/bin/terrahelp && chmod +x /usr/local/bin/terrahelp' 'terrahelp --version'
-                        ;;
-                    "terragrunt")
-                        install_tool "$tool" 'curl -OL https://github.com/gruntwork-io/terragrunt/releases/download/v0.45.8/terragrunt_linux_amd64 && mv terragrunt_linux_amd64 /usr/local/bin/terragrunt && chmod +x /usr/local/bin/terragrunt' 'terragrunt --version'
-                        ;;
-                    "kubectl")
-                        install_tool "$tool" 'curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && mv kubectl /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl' 'kubectl version --client --short'
-                        ;;
-                esac
-            else
-                echo "Please install $tool before proceeding."
-                exit 1
-            fi
-        fi
+    for tool_version in "${tool_versions[@]}"; do
+        IFS=':' read -r tool required_version <<< "$tool_version"
+        case $tool in
+            "aws")
+                aws_version="aws --version | awk 'NR==1{print \$1}' | cut -d'/' -f2"
+                install_tool "$tool" 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin' "$aws_version" "$required_version"
+                ;;
+            "helm")
+                helm_version="helm version --short | awk -F'[v+]' '/v/{print \$2}'"
+                install_tool "$tool" 'curl https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz -o helm.tar.gz && tar -zxvf helm.tar.gz && sudo mv linux-amd64/helm /usr/local/bin/' "$helm_version" "$required_version"
+                ;;
+            "terraform")
+                terraform_version='terraform version | awk '\''/Terraform/{gsub(/[^0-9.]/, "", $2); print $2}'\'''
+                install_tool "terraform" 'curl -LO "https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip" -o "terraform.zip" && unzip terraform.zip && sudo mv terraform /usr/local/bin/' "$terraform_version" "$required_version"
+                ;;
+            "terrahelp")
+                terrahelp_version='terrahelp --version | awk '\''/terrahelp version/ {print $3}'\'''
+                install_tool "$tool" 'curl -OL https://github.com/opencredo/terrahelp/releases/download/v0.4.3/terrahelp-linux-amd64 && mv terrahelp-linux-amd64 /usr/local/bin/terrahelp && chmod +x /usr/local/bin/terrahelp' "$terrahelp_version" "$required_version"
+                ;;
+            "terragrunt")
+                terragrunt_version='terragrunt --version | awk '\''/terragrunt version/ {gsub(/v/, "", $3); print $3}'\'''
+                install_tool "$tool" 'curl -OL https://github.com/gruntwork-io/terragrunt/releases/download/v0.45.8/terragrunt_linux_amd64 && mv terragrunt_linux_amd64 /usr/local/bin/terragrunt && chmod +x /usr/local/bin/terragrunt' "$terragrunt_version" "$required_version"
+                ;;
+            
+        esac
     done
 
     echo "All required tools are installed. Proceeding with the rest of the script..."
-    # Continue with the rest of your script...
 }
+
+
 
 # Kube_config directory setup and takes the backup of existing kubeconfig if exists
 setup_kube_config() {
@@ -113,7 +152,10 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export KUBE_CONFIG_PATH=$KUBE_CONFIG_PATH
-setup_kube_config
 
-# Validate and install required tools
 validate_tools
+setup_kube_config
+# Rest of the script related to terraform and deployment will start from here
+
+
+
