c218b2b83c9e6790b065a1af64ab5ea4210cee2a
Ravi Mula <ravismula@users.noreply.github.com>
2024-11-26 16:35:53 +0530

    #OBS-I338 spark worker fixes

--- helmcharts/kitchen/install.sh
+++ helmcharts/kitchen/install.sh
@@ -101,10 +101,11 @@ all)
     bash $0 monitoring
     bash $0 coreinfra
     bash $0 obsrvapis
-    bash $0 hudi
+    if $cloud_env == "aws"; then
+        bash $0 hudi
+    fi
     bash $0 obsrvtools
     bash $0 additional
-
     ;;
 reset)
     helm uninstall additional -n obsrv
