9806455cc93a015b37c99779dc173dd4a8aede88
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-12-31 10:15:01 +0530

    #OBS-I320: updated commands to v5

--- helmcharts/services/postgresql-migration/configs/migrations/03-obsrv/V5__dataset_management_ddl.sql
+++ helmcharts/services/postgresql-migration/configs/migrations/03-obsrv/V5__dataset_management_ddl.sql
@@ -9,3 +9,6 @@ ALTER TABLE connector_instances ADD COLUMN IF NOT EXISTS name TEXT;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO obsrv;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO obsrv;
+
+ALTER TABLE oauth_users ADD COLUMN is_owner BOOLEAN DEFAULT FALSE;
+UPDATE oauth_users SET is_owner=true WHERE user_name ='obsrv_admin';
