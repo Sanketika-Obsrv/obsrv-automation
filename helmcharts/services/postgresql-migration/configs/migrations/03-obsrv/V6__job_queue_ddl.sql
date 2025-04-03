045fd5b32c2272a8474b3ff03240129240598447
JeraldJF <jeraldj@sanketika.in>
2025-04-03 14:55:44 +0530

    feat #OBS-I615 : moved the add contraint for status while table creation

--- helmcharts/services/postgresql-migration/configs/migrations/03-obsrv/V6__job_queue_ddl.sql
+++ helmcharts/services/postgresql-migration/configs/migrations/03-obsrv/V6__job_queue_ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS job_queue (
     dataset_id VARCHAR(255) NOT NULL,  
     job_type VARCHAR(50) NOT NULL,  
     job_params JSONB NOT NULL,  
-    status VARCHAR(20) NOT NULL CHECK (status IN ('submitted', 'approved', 'rejected', 'in_progress', 'completed', 'failed', 'cancelled')),  
+    status VARCHAR(20) NOT NULL CHECK (status IN ('submitted', 'approved', 'rejected', 'in_progress', 'completed', 'failed', 'cancelled', 'queued', 'partial_success')),  
     priority INTEGER DEFAULT 3 CHECK (priority BETWEEN 1 AND 5),  
     progress DECIMAL(5,2) DEFAULT 0 CHECK (progress BETWEEN 0 AND 100),  
     resource_config JSONB DEFAULT '{}'::JSONB,  
@@ -16,11 +16,7 @@ CREATE TABLE IF NOT EXISTS job_queue (
     reviewed_at TIMESTAMP WITH TIME ZONE,  
     message TEXT DEFAULT '',  
     iteration INTEGER DEFAULT 0 CHECK (iteration BETWEEN 0 AND 3)  
-);  
-
-ALTER TABLE job_queue 
-ADD CONSTRAINT job_queue_status_check 
-CHECK (status IN ('submitted', 'approved', 'rejected', 'in_progress', 'completed', 'failed', 'cancelled', 'queued', 'partial_success'));
+);
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO obsrv;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO obsrv;
